7db173412eb547d9b066afb2fdd702e74adfa325
Luuk Verhoeven <luuk@ldesign.nl>
2023-03-07 19:31:48 +0100

    Fix code style issues

--- version.php
+++ version.php
@@ -19,7 +19,7 @@
  *
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  *
- * @package   moodle-tool_developer
+ * @package   tool_developer
  * @copyright 04/03/2023 Mfreak.nl | LdesignMedia.nl - Luuk Verhoeven
  * @author    Luuk Verhoeven
  **/
