95cdacdb98f2ba172e66d1fd58ba28faf2377a9e
Luuk Verhoeven <luuk@ldesign.nl>
2023-03-08 16:28:38 +0100

    Move sentry to the plugin

--- javascript/static-worker.js
+++ javascript/static-worker.js
@@ -13,8 +13,10 @@ const URLS_TO_CACHE = [
     /\/lib\/javascript\.php\/-1\/lib\//,
     /\/lib\/javascript\.php\/-1\/course\/dndupload/
 ];
+console.log('tool_developer - static-worker.js - v1.0.0');
 
 self.addEventListener('fetch', event => {
+
     const url = new URL(event.request.url);
     if (!URLS_TO_CACHE.some(regex => regex.test(url.pathname))) {
         return;
@@ -25,7 +27,7 @@ self.addEventListener('fetch', event => {
             .then(cache => cache.match(event.request))
             .then(response => {
                 if (response) {
-                    console.log(`Cache hit: ${url.pathname}`);
+                    // console.log(`Cache hit: ${url.pathname}`);
                     return response;
                 }
 
@@ -33,14 +35,14 @@ self.addEventListener('fetch', event => {
                 return fetch(event.request)
                     .then(response => {
                         if (!response || response.status !== 200 || response.type !== 'basic') {
-                            console.log(`Invalid response: ${url.pathname}`);
+                            // console.log(`Invalid response: ${url.pathname}`);
                             return response;
                         }
 
                         const responseToCache = response.clone();
                         caches.open(CACHE_NAME)
                             .then(cache => {
-                                console.log(`Adding response to cache: ${url.pathname}`);
+                                // console.log(`Adding response to cache: ${url.pathname}`);
                                 cache.put(event.request, responseToCache);
                             });
 
